12ead4c56f8950ddaeca56357d2fd41b0c7d516a
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2022-02-14 21:42:41 +0530

    Zomato Clone Completed

---
+++ src/components/common/header/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./header.css";
+import Logo from "../../../images/logo.png";
+import Profile from "../../../images/profile.jpg";
+
+const Header = () => {
+  return (
+    <div className="header max-width">
+      <img src={Logo} alt="aahar-logo" className="header-logo" />
+
+      <div className="header-right">
+        <div className="header-location-search-container">
+          <div className="location-wrapper">
+            <div className="location-icon-name">
+              <i className="fi fi-rr-marker absolute-center location-icon"></i>
+              <div>Bangalore</div>
+            </div>
+            <i className="fi fi-rr-caret-down absolute-center"></i>
+          </div>
+          <div className="location-search-separator"></div>
+          <div className="header-searchBar">
+            <i className="fi fi-rr-search absolute-center search-icon"></i>
+            <input
+              className="search-input"
+              placeholder="Search for restaurant, cuisine or a dish"
+            />
+          </div>
+        </div>
+
+        <div className="profile-wrapper">
+          <img src={Profile} alt="profile" className="header-profile-image" />
+          <span className="header-username">Shuvam</span>
+          <i className="fi fi-rr-angle-small-down absolute-center profile-options-icon"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
